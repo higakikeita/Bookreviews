1305b04811ee62c2bbbe8f547258e93e98478ef6
EC2 Default User <ec2-user@ip-172-31-47-73.ap-northeast-1.compute.internal>
2020-04-10 14:02:33 +0000

    マイグレーション修正

--- books/resources/views/home.blade.php
+++ books/resources/views/home.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+                    <button class='btn btn-primary'>Bootstrap</button>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
